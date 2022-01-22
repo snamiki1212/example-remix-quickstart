1f8f0371d40c3f39466ee7501cd801d3364ee21b
snamiki1212 <snamiki1212@gmail.com>
2022-01-22 07:20:04 -0800

    refactor: posts page

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,22 +1,9 @@
 import { Link, useLoaderData } from "remix";
-
-export type Post = {
-  slug: string;
-  title: string;
-};
+import { getPosts } from "~/posts";
+import type { Post } from "~/posts";
 
 export const loader = () => {
-  const posts: Post[] = [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
-  return posts;
+  return getPosts();
 };
 
 export default function Posts() {
